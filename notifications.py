a5f4773617040bdc87329df0439dfc3d423540c5
Łukasz Gendosz <genda1231@gmail.com>
2024-11-27 23:09:33 +0100

    Replace config with pydantic settings

--- notifications.py
+++ notifications.py
@@ -1,41 +1,44 @@
-from dotenv import dotenv_values
+import logging
+from datetime import datetime
+
 import smtplib
+from discordwebhook import Discord
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from datetime import datetime
-from discordwebhook import Discord
-import logging
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+from config import config
 
-config = dotenv_values(".env")
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
 logger = logging.getLogger(__name__)
 
 
 def send_mail(subject, body, to):
-    logger.info('sending email')
+    logger.info("sending email")
     try:
         msg = MIMEMultipart()
-        msg['From'] = config['MAIL_LOGIN']
-        msg['To'] = to
-        msg['Subject'] = subject
-        msg.attach(MIMEText(body, 'plain'))
+        msg["From"] = config.MAIL_LOGIN
+        msg["To"] = to
+        msg["Subject"] = subject
+        msg.attach(MIMEText(body, "plain"))
 
-        server = smtplib.SMTP_SSL(
-            config['MAIL_SERVER_IP'], config['MAIL_SERVER_PORT'])
+        server = smtplib.SMTP_SSL(config.MAIL_SERVER_IP, config.MAIL_SERVER_PORT)
         server.ehlo()
-        server.login(config['MAIL_LOGIN'], config['MAIL_PASSWORD'])
+        server.login(config.MAIL_LOGIN, config.MAIL_PASSWORD)
         server.send_message(msg)
         server.quit()
     except Exception as e:
-        logger.error(f'error occurred: {e}')
+        logger.error(f"error occurred: {e}")
 
 
 def send_discord_message(message):
-    logger.info('sending discord message')
+    logger.info("sending discord message")
     try:
-        discord = Discord(url=config["DISCORD_WEBHOOK"])
+        discord = Discord(url=config.DISCORD_WEBHOOK)
         discord.post(content=message)
     except Exception as e:
-        logger.error(f'error occurred: {e}')
+        logger.error(f"error occurred: {e}")
